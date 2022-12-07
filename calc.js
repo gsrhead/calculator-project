b86a262c35f0ab66ff66011c4891cbde66face1b
gsrhead <s2k9krpm00@gmail.com>
2022-12-07 18:52:36 -0500

    calculator

--- calc.js
+++ calc.js
@@ -6,25 +6,17 @@ buttons.forEach((item) => {
     if (item.id == "clear") {
       display.innerText = "";
     } else if (item.id == "backspace") {
+      
       let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
+      display.innerText = string.substr(0, string.length - 100);
+
     } else if (display.innerText != "" && item.id == "equal") {
+
       display.innerText = eval(display.innerText);
     } else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
+
     } else {
       display.innerText += item.id;
     }
   };
 });
-
-const themeToggleBtn = document.querySelector(".theme-toggler");
-const calculator = document.querySelector(".calculator");
-const toggleIcon = document.querySelector(".toggler-icon");
-let isDark = true;
-themeToggleBtn.onclick = () => {
-  calculator.classList.toggle("dark");
-  themeToggleBtn.classList.toggle("active");
-  isDark = !isDark;
-};
